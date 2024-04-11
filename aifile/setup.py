e24270372fc041683e20300165351db194473601
SahajA25 <143540034+SahajA25@users.noreply.github.com>
2024-04-11 17:55:45 +0530

    Update setup.py

--- aifile/setup.py
+++ aifile/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aifile',
-    version='0.3',
+    version='0.5',
     packages=find_packages(),
     install_requires=[
         'scikit-learn',
